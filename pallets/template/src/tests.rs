bb1851b78cd7e5bc8c72c50ea06629315147bd78
dmitry dulesov <dmitry.dulesov@gmail.com>
2020-12-03 19:35:38 +0300

    add weight attributes to dispatches; add no-pay, conditional pay dispatches; add tests

--- pallets/template/src/tests.rs
+++ pallets/template/src/tests.rs
@@ -1,5 +1,11 @@
 use crate::mock::*;
-use frame_support::{assert_noop, assert_ok};
+use frame_support::{
+    assert_noop, assert_ok,
+    dispatch::{DispatchInfo, DispatchResultWithPostInfo, GetDispatchInfo},
+    traits::UnfilteredDispatchable,
+    weights::{constants::RocksDbWeight, Pays, Weight},
+};
+
 // Learn more about testing substrate runtime modules
 // https://substrate.dev/docs/en/knowledgebase/runtime/tests
 // type Module = super::Module<Test>;
@@ -18,6 +24,42 @@ fn events() -> Vec<Event> {
 #[allow(dead_code)]
 fn last_event() -> Event {
     System::events().pop().expect("Event expected").event
+}
+
+#[test]
+fn it_default_pallet_transaction_payment_multiplier() {
+    use frame_support::sp_runtime::{traits::Convert, FixedPointNumber, FixedU128};
+
+    type Multiplier = FixedU128;
+    let multiplier: Multiplier = Multiplier::saturating_from_integer(1);
+
+    let new_multiplier = <() as Convert<Multiplier, Multiplier>>::convert(multiplier);
+    println!(
+        "default multiplier convert from {:} to {:}",
+        multiplier, new_multiplier
+    );
+
+    assert_eq!(new_multiplier, 0.into());
+}
+
+#[test]
+#[cfg(feature = "payment")]
+fn it_identity_pallet_transaction_payment_multiplier() {
+    use frame_support::sp_runtime::{traits::Convert, FixedPointNumber, FixedU128};
+
+    type Multiplier = FixedU128;
+    let multiplier: Multiplier = Multiplier::saturating_from_integer(1);
+
+    let new_multiplier = <crate::payment::IdentityMultiplierUpdater as Convert<
+        Multiplier,
+        Multiplier,
+    >>::convert(multiplier);
+    println!(
+        "identiry multiplier convert from {:} to {:}",
+        multiplier, new_multiplier
+    );
+
+    assert_eq!(new_multiplier, multiplier);
 }
 
 #[test]
@@ -71,7 +113,6 @@ fn it_disable_account() {
             2,
             super::USER_ROLE
         ));
-
         assert_ok!(TemplateModule::account_disable(Origin::signed(1), 2));
     });
 }
@@ -104,6 +145,8 @@ fn it_try_create_by_user() {
 #[test]
 fn it_balance() {
     new_test_ext().execute_with(|| {
+        //total_issuance
+        assert_eq!(<pallet_balances::Module<Test>>::total_issuance(), 100000);
         assert_eq!(<pallet_balances::Module<Test>>::free_balance(1), 100000);
         assert_eq!(<pallet_balances::Module<Test>>::free_balance(2), 0);
         assert_ok!(TemplateModule::account_add(
@@ -120,5 +163,78 @@ fn it_balance() {
 
         assert_eq!(<pallet_balances::Module<Test>>::free_balance(1), 50000);
         assert_eq!(<pallet_balances::Module<Test>>::free_balance(2), 50000);
+        assert_eq!(<pallet_balances::Module<Test>>::total_issuance(), 100000);
+        assert_ok!(<pallet_balances::Module<Test>>::transfer(
+            Origin::signed(1),
+            2,
+            49990
+        ));
+        // Account 1 has been removed from balances and dust remaining 10
+        assert_eq!(<pallet_balances::Module<Test>>::total_issuance(), 99990);
+    });
+}
+
+#[test]
+fn it_dispatchable_weight() {
+    // pre-dispatch weights
+    fn assert_dispatch(call: crate::Call<Test>, weight: Weight, pay: Pays) -> DispatchInfo {
+        let dispatch_info = call.get_dispatch_info();
+
+        assert_eq!(dispatch_info.weight, weight);
+        assert_eq!(dispatch_info.pays_fee, pay);
+        dispatch_info
+    }
+    let call = crate::Call::<Test>::account_add(3, 1);
+    assert_dispatch(call, 10_000_000 + RocksDbWeight::get().writes(1), Pays::Yes);
+
+    let call = crate::Call::<Test>::update_something(3);
+    assert_dispatch(call, 10_000_000, Pays::Yes);
+
+    // post-dispatch weights
+    fn assert_call(
+        origin: Origin,
+        call: crate::Call<Test>,
+        weight: Weight,
+        pay: Pays,
+    ) -> DispatchResultWithPostInfo {
+        let dispatch_info = call.get_dispatch_info();
+        let dispatch_post_info = call.dispatch_bypass_filter(origin)?;
+
+        assert_eq!(dispatch_post_info.pays_fee(&dispatch_info), pay);
+        assert_eq!(
+            dispatch_post_info.calc_actual_weight(&dispatch_info),
+            weight
+        );
+        Ok(dispatch_post_info)
+    }
+
+    new_test_ext().execute_with(|| {
+        let call = crate::Call::<Test>::update_something(300);
+        assert_ok!(assert_call(Origin::signed(1), call, 10000000, Pays::Yes));
+
+        let call = crate::Call::<Test>::update_something(100);
+        assert_ok!(assert_call(Origin::signed(1), call, 10000000, Pays::No));
+
+        let call = crate::Call::<Test>::account_add(3, 1);
+        assert_ok!(assert_call(
+            Origin::signed(1),
+            call,
+            10000000 + RocksDbWeight::get().writes(1),
+            Pays::Yes
+        ));
+
+        let call = crate::Call::<Test>::account_disable(3);
+        assert_ok!(assert_call(
+            Origin::signed(1),
+            call,
+            10000000 + RocksDbWeight::get().reads_writes(1, 1),
+            Pays::Yes
+        ));
+
+        let call = crate::Call::<Test>::update_something(300);
+        assert_noop!(
+            assert_call(Origin::signed(2), call, 10000000, Pays::Yes),
+            Error::NotAuthorized
+        );
     });
 }
