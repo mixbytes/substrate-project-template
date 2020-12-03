f86727c469cdd40b68f0dc271c76721dbe810c02
dmitry dulesov <dmitry.dulesov@gmail.com>
2020-12-03 22:36:54 +0300

    beautify js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,13 @@
 // A demonstration of interacting with custom RPCs using Polkadot js API
 
-const { ApiPromise, WsProvider } = require('@polkadot/api');
+const {
+    ApiPromise,
+    WsProvider
+} = require('@polkadot/api');
 const dotenv = require('dotenv');
-const { readFileSync } = require('fs');
+const {
+    readFileSync
+} = require('fs');
 dotenv.config();
 
 const ws_url = process.env.ws_url || "ws://localhost:9944";
@@ -12,30 +17,22 @@ const types = JSON.parse(readFileSync(process.env.types || "../pallets/types.jso
 // Construct parameters for API instance
 const wsProvider = new WsProvider(ws_url);
 
-const rpc = {
-  sumStorage: {
-    getSum: {
-      description: "Gets the sum of the two storage values in sum-storage pallet via a runtime api.",
-      params: [],
-      type: "u32",
-    }
-  }
-}
+const rpc = {}
 
 async function main() {
-  // Construct the actual api
-  const api = await ApiPromise.create({
-    provider: wsProvider,
-    types,
-    rpc,
-  });
+    // Construct the actual api
+    const api = await ApiPromise.create({
+        provider: wsProvider,
+        types,
+        rpc,
+    });
 
-  const now = ( await api.query.timestamp.now() ).toNumber();
-  const nextFeeMultiplier = ( await api.query.transactionPayment.nextFeeMultiplier() );
-  const storageVersion = ( await api.query.transactionPayment.storageVersion() );
+    const now = (await api.query.timestamp.now()).toNumber();
+    const nextFeeMultiplier = (await api.query.transactionPayment.nextFeeMultiplier());
+    const storageVersion = (await api.query.transactionPayment.storageVersion());
 
-  console.log(`timestamp.now ${now}`);
-  console.log(`transactionPayment.nextFeeMultiplier is ${nextFeeMultiplier}`);
-  console.log(`transactionPayment.storageVersion is ${storageVersion}`);
+    console.log(`timestamp.now ${now}`);
+    console.log(`transactionPayment.nextFeeMultiplier is ${nextFeeMultiplier}`);
+    console.log(`transactionPayment.storageVersion is ${storageVersion}`);
 }
 main().catch(console.error).finally(() => process.exit());
